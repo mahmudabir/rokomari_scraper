70e0376fc39974e8ffae4e9446414c0395aa49c2
Abir Mahmud <amabirmahmud@gmail.com>
2024-02-04 00:00:01 +0600

    book model

---
+++ models/book.py
@@ -0,0 +1,26 @@
+from models.book_category import BookCategory
+
+
+class Book:
+    def __init__(
+        self,
+        title: str,
+        author: str,
+        isAvailable: bool,
+        originalPrice: float,
+        currentPrice: float,
+        imageUrl: str,
+        bookUrl: str,
+        category: BookCategory,
+    ):
+        self.title = title.strip()
+        self.author = author.strip()
+        self.isAvailable = isAvailable
+        self.originalPrice = originalPrice
+        self.currentPrice = currentPrice
+        self.imageUrl = imageUrl.strip()
+        self.bookUrl = bookUrl.strip()
+        self.category = category
+
+    def __str__(self):
+        return f"{self.title} By {self.author}"
